2c07d0efd8ae2ba5a953860346c02937353427c8
parthitpatel <parthitpatel@gmail.com>
2020-08-25 13:38:26 +0530

    Added Rotate and Revolve sripts Make two spheres rotate around a third sphere This marks the end of the revision of the Unity basics

---
+++ UnityBasics/Assets/Scripts/RotateSpheres.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSpheres : MonoBehaviour
+{
+    Vector3 movement;
+    public float xi, yi, zi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3(xi,yi,zi);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(movement);
+    }
+}
