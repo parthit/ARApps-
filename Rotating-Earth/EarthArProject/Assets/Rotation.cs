05b8a008f9c218305dc757de508589926d2e0878
parthitpatel <parthitpatel@gmail.com>
2020-08-25 12:13:23 +0530

    Added a simple demo project

---
+++ Rotating-Earth/EarthArProject/Assets/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	public float speed = 150f;
+
+	public bool Rotating = false;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, Time.deltaTime * speed, 0, Space.Self);
+	}
+}
---
+++ Rotating-Earth/README.md
@@ -0,0 +1,5 @@
+# AR App that projects a rotating earth on a writing pad
+
+## Demo app to check out how to import models and write simple C# scripts
+
+## Can be expanded later
