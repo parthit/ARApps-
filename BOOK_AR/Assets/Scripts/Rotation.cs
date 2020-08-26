89f161030055b0053542ed56b4116c9f132cf176
parthitpatel <parthitpatel@gmail.com>
2020-08-26 15:40:49 +0530

    created a BOOK AR Project

---
+++ BOOK_AR/Assets/Scripts/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    Vector3 movement;
+    public float xi, yi, zi;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3(xi,yi,zi);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(movement);
+    }
+    
+}
