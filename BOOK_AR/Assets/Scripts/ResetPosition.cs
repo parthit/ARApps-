7134307f4d980870f3f582a2127edaf26f4bf3d4
parthitpatel <parthitpatel@gmail.com>
2020-08-27 15:24:11 +0530

    Added the script that lauches the plane scene only when the target image is detected Added limit (colliders) for the plane so that it respawns after reaching a certain positoin Created an APK file

---
+++ BOOK_AR/Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 originalpos;
+    void Start()
+    {
+        originalpos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collide){
+        if(collide.gameObject.CompareTag("CLOUDWALL")){
+            transform.localPosition = originalpos;
+        }
+        else if(collide.gameObject.CompareTag("AEROPLANEWALL")){
+            transform.localPosition = originalpos;
+        }
+    }
+}
