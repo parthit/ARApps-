c201edbf68c53af2572a0502f44fd1815144f248
parthitpatel <parthitpatel@gmail.com>
2020-10-23 15:54:29 +0530

    Created the finished App

---
+++ MyFriends/Assets/datafiles/Scripts/ARFriendsScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ARFriendsScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject parthitgo, aditigo, riago;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if (vb.VirtualButtonName == "AditiVB")
+        {
+            aditigo.SetActive(true);
+            parthitgo.SetActive(false);
+            riago.SetActive(false);
+
+        }
+        else if(vb.VirtualButtonName == "ParthitVB")
+        {
+            aditigo.SetActive(false);
+            parthitgo.SetActive(true);
+            riago.SetActive(false);
+        }
+        else if(vb.VirtualButtonName == "RiaVB")
+        {
+            aditigo.SetActive(false);
+            parthitgo.SetActive(false);
+            riago.SetActive(true);
+        }
+        else
+        {
+            throw new UnityException(vb.VirtualButtonName + "Virtual Button not supported");
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Virtual Button released");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour[] vrb = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vrb.Length; i++)
+        {
+            vrb[i].RegisterEventHandler(this);
+        };
+
+        parthitgo.SetActive(false);
+        aditigo.SetActive(false);
+        riago.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
