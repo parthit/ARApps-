ff98ecf53eaa1c8b356acaedc6f28fe9ad99932a
parthitpatel <parthitpatel@gmail.com>
2020-10-23 12:23:21 +0530

    Created a simple Virtual Button App

---
+++ ARVirtualButtonBusinessCard/Assets/Scripts/VirtualButtonScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject spherego, cubego;
+    VirtualButtonBehaviour vrb;
+   
+    void Start()
+    {
+        vrb = GetComponentInChildren<VirtualButtonBehaviour>();
+        vrb.RegisterEventHandler(this);
+        cubego.SetActive(true);
+        spherego.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubego.SetActive(true);
+        spherego.SetActive(false);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubego.SetActive(false);
+        spherego.SetActive(true);
+    }
+}
